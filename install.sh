43ffee7eb46001561ca6635b9a79441d630f0555
tonyzzp <tonyzzp@qq.com>
2024-07-02 16:24:51 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-echo start build...
-go build -ldflags "-s -w" -trimpath .
-echo build complete
-echo copy to /usr/bin
-sudo cp xddns /usr/bin/xddns
-sudo chmod +x /usr/bin/xddns
+# echo start build...
+# go build -ldflags "-s -w" -trimpath .
+# echo build complete
+# echo copy to /usr/bin
+# sudo cp xddns /usr/bin/xddns
+# sudo chmod +x /usr/bin/xddns
 
+echo start build and install
+go install -ldflags="-s -w" -trimpath -v .
